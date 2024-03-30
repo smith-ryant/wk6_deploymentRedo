dad3e4cc1d34909ab11490e66fcb9e5dadb1b78b
Ryan Smith <67011453+smith-ryant@users.noreply.github.com>
2024-03-29 18:54:34 -0600

    wk6_deploymentDemo minor changes

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express(); // create express app
 app.use(express.static(`${__dirname}/public`)); // serve static files
 
 app.listen(4000, () => {
-  console.log("server started on http://localhost:4000");
-}); // server listens on port 4000
+  console.log("server started on 4000");
+}); // server listens on port 400
